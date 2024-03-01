a7a206febe1cb920f5889048ce22138d9e598eff
Jay.develops <jaymations1234@gmail.com>
2024-02-29 19:48:48 -0800

    Created LatestGuests.tsx component on the dashboard

--- app/dashboard/LatestGuests.tsx
+++ app/dashboard/LatestGuests.tsx
@@ -1,12 +1,50 @@
 import {Card, CardDescription, CardHeader, CardTitle} from "@/components/ui/card";
+import {Table, TableBody, TableCell, TableRow} from "@/components/ui/table";
+import prisma from "@/prisma/client";
+import Link from "next/link";
+import GuestStatusBadge from "@/components/GuestStatusBadge";
+import {Avatar, AvatarFallback} from "@/components/ui/avatar";
 
-export default function LatestGuests() {
+export default async function LatestGuests() {
+    const getLocations = await prisma.location.findMany()
+
+    const latestGuests = await prisma.guest.findMany({
+        orderBy: {
+            signedIn: "asc",
+        },
+        take: 5,
+    })
     return (
         <Card>
             <CardHeader>
                 <CardTitle>Latest Guests</CardTitle>
                 <CardDescription>Overview of the latest guests activity.</CardDescription>
             </CardHeader>
+
+            <Table>
+                <TableBody>
+                    {latestGuests.map((guest) => (
+                        <TableRow key={guest.id}>
+                            <TableCell>
+                                <div className="flex flex-row justify-between">
+                                    {/*Guest full name and their status badge*/}
+                                    <div className="flex flex-col items-start gap-2">
+                                        <Link href={`/guests/${guest.id}`} className="font-bold">
+                                            {guest.fullName}
+                                        </Link>
+
+                                        <GuestStatusBadge status={guest.status}/>
+                                    </div>
+
+                                    <Avatar>
+                                        <AvatarFallback>CN</AvatarFallback>
+                                    </Avatar>
+                                </div>
+                            </TableCell>
+                        </TableRow>
+                    ))}
+                </TableBody>
+            </Table>
         </Card>
     )
 }
